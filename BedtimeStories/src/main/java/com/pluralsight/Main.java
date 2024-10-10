85865cb604b0d82eed9b96f9362bcaf6e2aa8cbf
sharianakram <sharianakram0503@gmail.com>
2024-10-09 20:47:28 -0400

    completed exercise

---
+++ BedtimeStories/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,57 @@
+package com.pluralsight;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        do {
+            try {
+                System.out.println("Goldilocks");
+                System.out.println("Hansel and Gretel");
+                System.out.println("Mary had a little lamb");
+                String userInput = Console.PromptForString("Please choose a story from the choices above: ");
+                userInput = (userInput.toLowerCase()).replace(' ', '_');
+                FileInputStream fis = new FileInputStream(userInput + ".txt");
+                Scanner scanner = new Scanner(fis);
+                int lineNumber = 1;
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    System.out.println(lineNumber + ": " + line);
+                    lineNumber++;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("File Not found");
+            }
+        }while(true);
+    }
+
+
+    public static String getStory(){
+        System.out.println("1 - Goldilocks");
+        System.out.println("2 - Hansel and Gretel");
+        System.out.println("3 - Mary had a little lamb");
+        String userInput = Console.PromptForString("Please choose a story from the choices above: ");
+        do{
+            if (Objects.equals(userInput, "1") || userInput.equalsIgnoreCase("Goldilocks")){
+                return "goldilocks.txt";
+
+            }else if(Objects.equals(userInput, "2") || userInput.equalsIgnoreCase("Hansel and Gretel")){
+                return "hansel_and_gretel.txt";
+
+            }else if (Objects.equals(userInput, "3") || userInput.equalsIgnoreCase("Mary had a little lamb")){
+                return "mary_had_a_little_lamb.txt";
+
+            }else{
+                userInput = Console.PromptForString("Story not available, PLease choose an option from the list provided: ");
+            }
+
+        }while(true);
+    }
+}
+
---
+++ BufferedReader/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,55 @@
+package com.pluralsight;
+
+public class Employee {
+    private int employeeId;
+    private String name;
+    private double hoursWorked;
+    private double payRate;
+
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.hoursWorked = hoursWorked;
+        this.payRate = payRate;
+    }
+
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public double getPayRate() {
+        return payRate;
+    }
+
+
+    public void setHoursWorked(double hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+
+    public void setPayRate(double payRate) {
+        this.payRate = payRate;
+    }
+
+    public double getGrossPay(){
+        double overtimeHours = hoursWorked - 40;
+        double overtimePay;
+        double totalPay;
+        if (overtimeHours > 0){
+            overtimePay = overtimeHours * (1.5 * payRate);
+            totalPay = overtimePay + (40 * payRate);
+        }else{
+            totalPay = hoursWorked * payRate;
+        }
+        return totalPay;
+
+    }
+}
